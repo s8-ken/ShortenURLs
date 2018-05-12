0094ba5638bfd6e8b6e41a0bbdebee0b03297f97
s8_ken <shibaken@tmtv.ne.jp>
2018-05-12 09:33:46 +0200

    delete my mistakes

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'static/Pages'
-  get 'static/home'
-  get 'static/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
